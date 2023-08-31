d3fe48848a33aa08a8e6fed56a73a7ae9ebe2c15
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2023-08-31 19:05:41 -0300

    chore: absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@src': './src',
+        },
+      },
+    ],
+  ],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
